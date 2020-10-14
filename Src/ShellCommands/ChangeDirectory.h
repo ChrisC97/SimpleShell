0a3c289eb0ef9a84c0580ee34f28d1c0b3501c2f
ChrisC97 <christopher.collins@bison.howard.edu>
2020-10-14 14:09:12 -0700

    Update ChangeDirectory.h

--- Src/ShellCommands/ChangeDirectory.h
+++ Src/ShellCommands/ChangeDirectory.h
@@ -14,5 +14,5 @@ void delete_ChangeDirectory(ShellCommand* const sCommandShellObj); //destructor
 
 // Check if the input given matches this command.
 bool ChangeDirectory_CheckInput(ShellCommand* commandObj, char * commandArg);
-// Handle the processing of this command.
+// Changes the current working directory of the shell.
 void ChangeDirectory_Process(ShellCommand* commandObj, int argc, char ** args);
