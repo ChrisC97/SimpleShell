23382c75469f93b639c31b18613df12b0fd9ca25
ChrisC97 <christopher.collins@bison.howard.edu>
2020-10-14 14:10:22 -0700

    Update PwdCmd.h

--- Src/ShellCommands/PwdCmd.h
+++ Src/ShellCommands/PwdCmd.h
@@ -14,5 +14,5 @@ void delete_PwdCmd(ShellCommand* const sCommandShellObj); //destructor
 
 // Check if the input given matches this command.
 bool PwdCmd_CheckInput(ShellCommand* commandObj, char * commandArg);
-// Handle the processing of this command.
+// Prints the current working directory.
 void PwdCmd_Process(ShellCommand* commandObj, int argc, char ** args);
